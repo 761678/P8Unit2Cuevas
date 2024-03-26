c51430db462a609acf20e4f09e92f255b13edd00
761678 <pedrocuevasarana123@gmail.com>
2024-03-26 00:04:58 -0700

    trying to fix some bugs

--- P8Unit2BasicGameplayCuevas/Assets/Scripts/PlayerController.cs
+++ P8Unit2BasicGameplayCuevas/Assets/Scripts/PlayerController.cs
@@ -1,20 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
-{
+{ 
     public float horizontalInput;
     public float verticalInput;
     public float speed = 10.0f;
     public float xRange = 10.0f;
     public float zRange = 10.0f;
 
-    public GameObject projectilePrefab; 
+    public GameObject projectilePrefab;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -22,18 +23,18 @@ public class PlayerController : MonoBehaviour
     {
         if (transform.position.x < -xRange)
         {
-            transform.position = new Vector3(-xRange,transform.position.y, transform.position.z);
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
         }
 
         if (transform.position.x > xRange)
         {
-            transform.position = new Vector3(xRange,transform.position.y, transform.position.z);
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
 
         verticalInput = Input.GetAxis("Vertical");
-        transform.Translate(Vector3.forward *  verticalInput * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
--- P8Unit2BasicGameplayCuevas/Assets/Scripts/SpawnManager.cs
+++ P8Unit2BasicGameplayCuevas/Assets/Scripts/SpawnManager.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
+    public GameObject[] sideAnimals;
     private float spawnRangeX = 20;
     private float spawnPosZ = 20;
+
     private float startDelay = 2;
     private float spawnInterval = 1.5f;
 
@@ -14,12 +16,14 @@ public class SpawnManager : MonoBehaviour
     void Start()
     {
         InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
+        InvokeRepeating("SpawnAnimalLeft", startDelay, spawnInterval);
+        InvokeRepeating("SpawnAnimalRight", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 
     void SpawnRandomAnimal()
@@ -28,5 +32,19 @@ public class SpawnManager : MonoBehaviour
         Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
 
         Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+    }
+
+    void SpawnAnimalLeft()
+    {
+        Vector3 spawnPos = new Vector3(-25, 0, Random.Range(5, 15));
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Instantiate(animalPrefabs[animalIndex], spawnPos, Quaternion.LookRotation(Vector3.right));
+    }
+     
+    void SpawnAnimalRight()
+    {
+        Vector3 spawnPos = new Vector3(25, 0, Random.Range(5, 15));
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Instantiate(animalPrefabs[animalIndex], spawnPos, Quaternion.LookRotation(Vector3.left));
     }
 }
