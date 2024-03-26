bd393a3a754999d2e8d91cb9fbdf5bbd0860721f
761678 <pedrocuevasarana123@gmail.com>
2024-03-26 12:10:29 -0700

    Done
    Well most of it

--- P8Unit2BasicGameplayCuevas/Assets/Scripts/DetectCollision.cs
+++ P8Unit2BasicGameplayCuevas/Assets/Scripts/DetectCollision.cs
@@ -6,7 +6,12 @@ public class DetectCollision : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        Destroy(gameObject);
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("Player Get Hit, Game Over");
+        }
+        else
+            Destroy(gameObject);
         Destroy(other.gameObject);
     }
 }
